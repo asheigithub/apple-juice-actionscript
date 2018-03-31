324971ea2d669e385ffc769e6c314ca05093a516
wxy <wuxiaoyu@zhuqigame.com>
2018-03-31 14:34:04 +0800

    jar 更新

--- ASRuntimeSDK/build.bat
+++ ASRuntimeSDK/build.bat
@@ -1,7 +1,7 @@
 
 @set path=%path%;C:\Program Files (x86)\Java\jdk1.8.0_131\bin
 
-javac -s src src\com\adobe\flash\compiler\clients\MXMLC.java -d classes
+javac -source 1.4 -target 1.4 -s src src\com\adobe\flash\compiler\clients\MXMLC.java -d classes
 jar cvfm mxmlc-cli.jar MANIFEST.MF -C classes com\adobe\flash\compiler\clients\MXMLC.class
 
 pause
